fae4bd50dc6da1a34cfca9e9ed8bedceae19944a
ThalapathySiva <sivaramsiva16@gmail.com>
2019-03-09 16:11:28 +0530

    Map Implemented Successfully

--- app/src/main/java/com/example/crowdup/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/crowdup/Activities/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.example.crowdup.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.crowdup.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.crowdup.Alumni
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+    private var firebaseauth:FirebaseAuth?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(com.example.crowdup.R.layout.activity_login)
+        firebaseauth = FirebaseAuth.getInstance()
+        setOnClickListener()
+    }
+    fun setOnClickListener() {
+        signIn.setOnClickListener {
+            val intent = Intent(this, Alumni::class.java)
+            startActivity(intent)
+        }
     }
 }
